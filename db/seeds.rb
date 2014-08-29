4446a1bdc742d3df3dfa7092329133b8d9a25f4b
Philippine Crawfish <phillipcrawford3@gmail.com>
2014-08-29 14:25:51 -0700

    Added faker gem Added seed file for comments, questions, answers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+  Question.create(:title => Faker::Lorem.word, :body => Faker::Lorem.sentence, :vote_count => rand(1..50) )
+end
+
+15.times do
+  answer = Answer.create(:title => Faker::Lorem.word, :body => Faker::Lorem.sentence, :vote_count => rand(1..50))
+  question = Question.find(rand(1..10))
+  question.answers << answer
+end
+
+20.times do
+  comment = Comment.create(:body => Faker::Lorem.sentence, :vote_count => rand(1..50))
+  answer = Answer.find(rand(1..15))
+  answer.comments << comment
+end
+
+20.times do
+  comment = Comment.create(:body => Faker::Lorem.sentence, :vote_count => rand(1..50))
+  question = Question.find(rand(1..10))
+  question.comments << comment
+end
