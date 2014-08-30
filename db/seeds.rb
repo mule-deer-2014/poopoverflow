e4c57f829f495d5e3e5e685272fc80c23e7b2545
Cassidy Clawson & I love turtles <pair+cassidyclawson.ninachurch@devbootcamp.com>
2014-08-30 15:13:06 -0700

    Seeds DB with Questions, Answers, Comments, and Votes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 10.times do
-  Question.create(:title => Faker::Lorem.word, :body => Faker::Lorem.sentence, :vote_count => rand(1..50) )
+  Question.create(:title => Faker::Lorem.word, :body => Faker::Lorem.sentence)
 end
 
-15.times do
-  answer = Answer.create(:title => Faker::Lorem.word, :body => Faker::Lorem.sentence, :vote_count => rand(1..50))
+30.times do
+  answer = Answer.create(:body => Faker::Lorem.sentence)
   question = Question.find(rand(1..10))
   question.answers << answer
 end
 
 20.times do
-  comment = Comment.create(:body => Faker::Lorem.sentence, :vote_count => rand(1..50))
-  answer = Answer.find(rand(1..15))
+  comment = Comment.create(:body => Faker::Lorem.sentence)
+  answer = Answer.find(rand(1..30))
   answer.comments << comment
 end
 
 20.times do
-  comment = Comment.create(:body => Faker::Lorem.sentence, :vote_count => rand(1..50))
+  comment = Comment.create(:body => Faker::Lorem.sentence)
   question = Question.find(rand(1..10))
   question.comments << comment
+end
+
+20.times do
+  comment = Comment.create(:body => Faker::Lorem.sentence)
+  question = Question.find(rand(1..10))
+  question.comments << comment
+end
+
+30.times do
+  vote = Vote.create(:vote_value => [-1,1,1,1].sample)
+  question = Question.find(rand(1..10))
+  question.votes << vote
+end
+
+30.times do
+  vote = Vote.create(:vote_value => [-1,1,1,1].sample)
+  comment = Comment.find(rand(1..20))
+  comment.votes << vote
+end
+
+30.times do
+  vote = Vote.create(:vote_value => [-1,1,1,1].sample)
+  answer = Answer.find(rand(1..30))
+  answer.votes << vote
 end
