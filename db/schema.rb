e50d5a7b5ffd10d0914fbb636df972e8652497bb
tommy.dugger78@gmail.com <tommy.dugger78@gmail.com>
2014-08-30 12:57:17 -0700

    Adds new migrated schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -11,29 +11,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140830024416) do
+ActiveRecord::Schema.define(version: 20140830195312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: true do |t|
-    t.string  "title"
-    t.string  "body"
-    t.integer "vote_count",  default: 0
-    t.integer "question_id"
+    t.string   "body"
+    t.integer  "question_id"
+    t.integer  "user_id"
+    t.datetime "updated_at"
+    t.datetime "created_at"
   end
 
   create_table "comments", force: true do |t|
-    t.string  "body"
-    t.integer "vote_count",  default: 0
-    t.integer "question_id"
-    t.integer "answer_id"
+    t.string   "body"
+    t.integer  "user_id"
+    t.string   "comment_to_type"
+    t.integer  "comment_to_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "questions", force: true do |t|
-    t.string  "title"
-    t.string  "body"
-    t.integer "vote_count", default: 0
+    t.string   "title"
+    t.string   "body"
+    t.integer  "user_id"
+    t.integer  "best_answer_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
@@ -53,5 +59,14 @@ ActiveRecord::Schema.define(version: 20140830024416) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "votes", force: true do |t|
+    t.integer  "vote_value"
+    t.integer  "user_id"
+    t.string   "vote_for_type"
+    t.integer  "vote_for_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
