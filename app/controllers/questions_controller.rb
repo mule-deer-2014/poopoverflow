48fd3f6b27a26edf395eff7e50eafda62d1ae155
kbasma <katia.bazzi@gmail.com>
2014-08-29 17:15:21 -0700

    added create and new methods to questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,26 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    respond_to do |format|
+      if @question.save
+        # format.json { render json: {question: @question}, status: :created, location: @question }
+        # format.json { render json: {html: render partial 'new_question', locals: {question: @question} }}
+        format.html {render '_new_question', locals: {question: @question}, layout: false }
+      else
+        render :new
+      end
+    end
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end 
 end
