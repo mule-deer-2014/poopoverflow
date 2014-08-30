705ff73d2b338af86aae45ee49458aac2667621f
tommy.dugger78@gmail.com <tommy.dugger78@gmail.com>
2014-08-30 14:29:19 -0700

    Adds empty controller actions for creating and deleting a question.  Fixes show view file for question to remove vote_count

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,4 +6,13 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
 end
