99ab36736b1ca706182bdb4155391a543acc50d3
Frank Shaw <fhshaw7@gmail.com>
2014-08-31 15:27:25 -0700

    created route to answering questions

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,8 +1,18 @@
 class AnswersController < ApplicationController
+  
   def new
+    @answer = Answer.new
+    @question = Question.find(params[:question_id])
   end
 
   def create
+    @answer = Answer.new(answers_params)
+
+    # if @answer.save
+    #   redirect_to root_path
+    # else
+    #   render :new
+    # end
   end
 
   def destroy
