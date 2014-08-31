479848bd05c866e2b8ad133d0f11fbb2e550779a
Alex Birdsall & Franky <pair+ambirdsall.bjjmeche@devbootcamp.com>
2014-08-30 19:02:25 -0700

    adds scoring module to all relevant models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,5 @@
 class Answer < ActiveRecord::Base
+  include Scoring
   belongs_to :user
   belongs_to :question
   has_one :question, foreign_key: :best_answer_id
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  include Scoring
   belongs_to :user
   belongs_to :comment_to, polymorphic: true
   has_many :votes, as: :vote_for
