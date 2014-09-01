8a679874bb5c57306009b491ec9b7444783f7369
Franky & Mr. Amazing <pair+bjjmeche.cerinman@devbootcamp.com>
2014-09-01 15:57:40 -0700

    added view logic to show resultData from question body form

---
+++ app/assets/javascripts/question/view.js
@@ -0,0 +1,9 @@
+var View = function (){
+  this.questionResultView = $("#result_view")
+}
+
+View.prototype = {
+  updateViewResult: function(resultData){
+    this.questionResultView.empty().append(resultData);
+  }
+}
